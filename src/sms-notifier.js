4f8cfeae5c6c03b016e73951d261036fa2fe1562
Jimmy Chen <jchen.tc@gmail.com>
2022-04-21 16:33:06 +0200

    Add comments

--- src/sms-notifier.js
+++ src/sms-notifier.js
@@ -1,5 +1,5 @@
 const accountSid = 'AC22edf3c824a4178b6fbe3b2438b7b51b';
-const authToken = '';
+const authToken = 'INSERT TOKEN HERE';
 const client = require('twilio')(accountSid, authToken);
 
 class SMSNotifier {
@@ -12,8 +12,8 @@ class SMSNotifier {
         client.messages
             .create({
                 body: message,
-                from: '+19206773875',
-                to: '+41792990136'
+                from: 'INSERT NUMBER HERE',
+                to: 'INSERT NUMBER HERE'
             })
             .then(message => console.log(message.sid));
     }
