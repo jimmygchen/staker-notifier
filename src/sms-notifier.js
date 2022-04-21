50bac6d16f24e8e45038352fd2910c51674b9cce
yury-dubinin <yury.dubinin@gmail.com>
2022-04-21 14:09:06 +0200

    added twilio

--- src/sms-notifier.js
+++ src/sms-notifier.js
@@ -1,3 +1,8 @@
+const accountSid = 'AC22edf3c824a4178b6fbe3b2438b7b51b';
+const authToken = '';
+const client = require('twilio')(accountSid, authToken);
+client.logLevel = 'debug';
+
 class SMSNotifier {
     constructor() {
 
@@ -5,6 +10,13 @@ class SMSNotifier {
 
     notify (message) {
         console.log(`SMS sent to ${message}`);
+        client.messages
+            .create({
+                body: 'Hello there!',
+                from: '+19206773875',
+                to: '+41792990136'
+            })
+            .then(message => console.log(message.sid));
     }
 }
 
