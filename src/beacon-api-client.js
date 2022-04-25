a0e60da3fdd49644c9a96c5fcb2c2c280ac2e3d9
Jimmy Chen <jchen.tc@gmail.com>
2022-04-25 01:28:13 +0100

    add support for multiple validators

--- src/beacon-api-client.js
+++ src/beacon-api-client.js
@@ -1,5 +1,7 @@
 const axios = require('axios')
 
+const validatorShortName = (pubkey) => pubkey.slice(2,9);
+
 class BeaconAPIClient {
     constructor(beaconAPIs) {
         this.beaconAPIs = beaconAPIs && beaconAPIs.split(',');
@@ -10,16 +12,17 @@ class BeaconAPIClient {
 
     async isBalanceReduced(pubkey) {
         const genesisTime = await this.getGenesisTime();
+        const validator = validatorShortName(pubkey);
 
         const currentSlot = Math.floor((new Date().getTime() / 1000 - genesisTime) / 12)
         const { data: currentEpochResp } = await this.getBeaconData(`/eth/v1/beacon/states/${currentSlot}/validators?id=${pubkey}`)
         const currentEpochBalance = currentEpochResp.data[0].balance
-        console.debug("Current balance: " + currentEpochBalance)
+        console.debug(`Current balance for validator ${validator}: ${currentEpochBalance}`)
 
         const previousEpochSlot = currentSlot - 32
         const { data: previousEpochResp } = await this.getBeaconData(`/eth/v1/beacon/states/${previousEpochSlot}/validators?id=${pubkey}`)
         const previousBalance = previousEpochResp.data[0].balance
-        console.debug("Previous balance: " + previousBalance)
+        console.debug(`Previous balance for validator ${validator}: ${previousBalance}`)
 
         return (Number(currentEpochBalance) <= Number(previousBalance))
     }
--- src/validator-balance-checker.js
+++ src/validator-balance-checker.js
@@ -8,19 +8,21 @@ class ValidatorBalanceChecker {
     }
 
     start(pubkeys) {
-        console.log(`Polling Beacon API every ${this.pollIntervalSeconds} seconds`)
+        console.log(`Polling Beacon API for ${pubkeys.length} validators every ${this.pollIntervalSeconds} seconds`)
         setInterval(this.checkBalances.bind(this, pubkeys), this.pollIntervalSeconds * 1000)
     }
 
-    async checkBalances(pubkeys) {
-        try {
-            const isBalanceReduced = await this.apiClient.isBalanceReduced(pubkeys)
-            if (isBalanceReduced) {
-                this.notifier.notify(`Balance for validator ${pubkeys} has reduced at ${new Date().toUTCString()}`)
+    checkBalances(pubkeys) {
+        pubkeys.forEach(async (pubkey) => {
+            try {
+                const isBalanceReduced = await this.apiClient.isBalanceReduced(pubkey)
+                if (isBalanceReduced) {
+                    this.notifier.notify(`Balance for validator ${pubkey} has reduced at ${new Date().toUTCString()}`)
+                }
+            } catch (err) {
+                console.error(`error retrieving balance for validator ${pubkey}: ${err}`)
             }
-        } catch (err) {
-            console.error(`error retrieving balance: ${err}`)
-        }
+        });
     }
 }
 
