71b386293e01f9b9ea59842a9e863f47cded08bc
yury-dubinin <yury.dubinin@gmail.com>
2022-12-30 10:57:22 +0100

    Update telegram-notifier.js
    removed config.js and if check

--- src/notifiers/telegram-notifier.js
+++ src/notifiers/telegram-notifier.js
@@ -1,8 +1,5 @@
 import {logger} from '../logger.js';
 import {TelegramBotApiClient} from '../telegram-bot-api-client.js';
-import {config} from "../config.js";
-
-const telegramBotClient = new TelegramBotApiClient(config.telegram.botURL, config.telegram.botToken);
 
 class TelegramNotifier {
     constructor(config) {
@@ -11,12 +8,8 @@ class TelegramNotifier {
     }
 
     notify(message) {
-        if (this.chatID) {
-            logger.info(`Sending telegram message to chat_id[${this.chatID}] via bot: ${message}`);
-            telegramBotClient.sendMessage(this.chatID, message).then(r => logger.info("Message sent to " + this.chatID));
-        } else {
-            logger.info(`Telegram message not configured`);
-        }
+        logger.info(`Sending telegram message to chat_id[${this.chatID}] via bot: ${message}`);
+        this.client.sendMessage(this.chatID, message).then(r => logger.info("Message sent to " + this.chatID));
     }
 
 }
